6fe114f39398819f0a0d0eb0196e5a4f8812e24c
edison.antru <edison@yarkan.pe>
2024-01-26 18:22:04 -0500

    viernes

--- src/components/ActionComp.vue
+++ src/components/ActionComp.vue
@@ -5,18 +5,22 @@
     <teleport to="#app">
       <ModalComp v-show="showModal" @close="showModal=false">
         <form @submit.prevent="submit">
+
           <div class="field">
             <label>Título</label>
             <input type="text" v-model="title">
           </div>
+
           <div class="field">
             <label>Monto</label>
             <input type="number" v-model="amount">
           </div>
+
           <div class="field">
             <label>Description</label>
             <textarea rows="4" v-model="description"></textarea>
           </div>
+
           <div class="field">
             <label>Tipo de movimiento: </label>
             <label class="radio-label">
@@ -26,27 +30,41 @@
             <label class="radio-label">
               <input type="radio" v-model="movementType" value="Gasto">
               <span>Gasto</span>
+              
             </label>
           </div>
+
           <div  class="action">
             <button>Agregar movimiento</button>
           </div>
+
         </form>
       </ModalComp>
     </teleport>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, defineEmits } from 'vue'
 import ModalComp from "./ModalComp.vue"
  
 const showModal = ref(false);
 const title = ref("");
 const amount = ref(0);
 const description = ref("");
-const movementType = ref("INgreso");
+const movementType = ref("Ingreso");
 
-const submit =( () => showModal.value = false)
+const emit = defineEmits(["create"])
+
+const submit =( () => {
+    showModal.value = false;
+    emit("create", {
+      title: title.value,
+      description: description.value,
+      amount: movementType.value === "Ingreso" ? amount.value : -amount.value,
+      time: new Date(),
+      id: new Date(),
+    });
+  })
 </script>
 
 <style scoped>
@@ -59,6 +77,7 @@ button {
   padding: 12px 60px;
   border-radius: 60px;
   box-sizing: border-box;
+  margin: 0;
 }
 
 form {
--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -1,19 +1,27 @@
 <template>
   <LayoutComp>
+    <!-- Header -->
     <template #header>
       <HeaderComp />
     </template>
+
+    <!-- Index -->
     <template #resume>
-      <Resume :label="label" :amount="amount" :totalAmount="20000">
-        <template #graphic> graphic </template>
+      <Resume :label="label" :amount="amount" :totalAmount="totalamount">
+        <template #graphic>
+          <GraphicComp :amounts="amounts"></GraphicComp>
+        </template>
         <template #action>
-          <ActionComp/>
+          <ActionComp @create="create"/>
         </template>
       </Resume>
     </template>
+
+    <!-- List movements -->
     <template #movements>
       <Movements :movements="movs"/>
     </template>
+
   </LayoutComp>
 </template>
 <script>
@@ -22,6 +30,7 @@ import LayoutComp from "./LayoutComp.vue";
 import Resume from "./Resume/IndexComp.vue";
 import Movements from "./Movements/IndexMov.vue";
 import ActionComp from "./ActionComp.vue";
+import GraphicComp from "./Resume/GraphicComp.vue";
 
 export default {
   components: {
@@ -29,29 +38,103 @@ export default {
     HeaderComp,
     Resume,
     Movements,
-    ActionComp
+    ActionComp,
+    GraphicComp
 },
   data() {
     return {
       amount: null,
+      label: null,
+      totalamount: "1000000",
       movs: [{
         id: 0,
         title: "Movimiento",
         description: "Antes de ganar casinl",
-        amount: 1000
+        amount: 100,
+        time: new Date("02-01-2024"),
       },{
         id: 1,
         title: "Paga",
         description: "Delivery diario",
-        amount: -240000
+        amount: 200,
+        time: new Date("02-01-2024"),
       },{
         id: 2,
-        title: "HAYSGHA",
+        title: "HAYSGHA 3",
         description: "Amgocamaramesi sdt asser",
-        amount: 1400
-      }
+        amount: 500,
+        time: new Date("02-01-2024"),
+      },{
+        id: 3,
+        title: "Movimi 4" ,
+        description: "Antes de ganar casinl",
+        amount: 200,
+        time: new Date("02-01-2024"),
+      },{
+        id: 4,
+        title: "Paga 5",
+        description: "Delivery diario",
+        amount: -400,
+        time: new Date("02-01-2024"),
+      },{
+        id: 5,
+        title: "Movimiento 6",
+        description: "Antes de ganar casinl",
+        amount: -600,
+        time: new Date("02-01-2024"),
+      },{
+        id: 6,
+        title: "Paga 7",
+        description: "Delivery diario",
+        amount: -300,
+        time: new Date("02-01-2024"),
+      },{
+        id: 7,
+        title: "Movimiento 8",
+        description: "Antes de ganar casinl",
+        amount: 100,
+        time: new Date("02-01-2024"),
+      },{
+        id: 8,
+        title: "Paga 9",
+        description: "Delivery diario",
+        amount: 300,
+        time: new Date("01-01-2024"),
+      },{
+        id: 9,
+        title: "Movimiento 10",
+        description: "Antes de ganar casinl",
+        amount: 500,
+        time: new Date("01-01-2024"),
+      },
     ]
     };
   },
+  computed : {
+    amounts() {
+      const lastDays = this.movs
+        .filter( m => {
+          const today = new Date();
+          const oldDate = today.setDate(today.getDate() - 30);
+
+          return m.time > oldDate
+        })
+        .map(m => m.amount)
+
+      return lastDays.map((m, i) => {
+        const lastMovements = lastDays.slice(0, i);
+
+        return lastMovements.reduce((suma, movement) => {
+          return suma + movement
+        }, 0);
+
+      }); 
+    }
+  },
+  methods: {
+    create(movement) {
+      this.movs.push(movement)
+    }
+  }
 };
 </script>
